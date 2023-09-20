24c3825d9be0554c64a2b4f08b9be3cfb96a1048
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-20 17:35:14 +0800

    added exception handler

--- contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.exception.NoContactException;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
@@ -19,11 +20,11 @@ public class ContactServiceImpl implements ContactService {
     
 
     // Stream the length of Array then find the contact based on the id
-    private int findIndexById(String id) {
+    private int findIndexById(String id) throws NoContactException {
         return IntStream.range(0, contactRepository.getContacts().size())
             .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(()-> new NoContactException());
     }
 
     @Override
@@ -37,17 +38,17 @@ public class ContactServiceImpl implements ContactService {
     } 
 
     @Override
-    public void updateContact(String id, Contact contact) {
+    public void updateContact(String id, Contact contact) throws NoContactException {
         contactRepository.updateContact(this.findIndexById(id), contact);
     }
 
     @Override
-    public void deleteContact(String id) {
+    public void deleteContact(String id) throws NoContactException {
         contactRepository.deleteContact(this.findIndexById(id));
     }
 
     @Override
-    public Contact getContactById(String id) {
+    public Contact getContactById(String id) throws NoContactException  {
         return contactRepository.getContact(String.valueOf(findIndexById(id)));
     }
 
--- contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMethodMappingNamingStrategy;
 
+import com.ltp.contacts.exception.NoContactException;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
@@ -31,8 +32,13 @@ public class ContactController {
 
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
-       Contact contact = contactService.getContactById(id);
-       return new ResponseEntity<>(contact, HttpStatus.OK);
+        try {
+            Contact contact = contactService.getContactById(id);
+            return new ResponseEntity<>(contact, HttpStatus.OK);
+        } catch (NoContactException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+       
     }
 
     @PostMapping("/contact")
@@ -43,14 +49,24 @@ public class ContactController {
 
     @PutMapping("/contact/{id}")
     public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
-        contactService.updateContact(id, contact);
-        return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
+        try {
+            contactService.updateContact(id, contact);
+            return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
+        } catch(NoContactException ex) {
+             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+      
     }
 
     @DeleteMapping("/contact/{id}")
     public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
-        contactService.deleteContact(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            contactService.deleteContact(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch(NoContactException ex) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+       
     }
 
 
